8e9265ded34ac8c507e9ad7b1a5fa24e83f23a5c
aosoriod <aosoriod@gmail.com>
2022-09-21 16:03:48 +0000

    [ADD] Basic Module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,25 @@
+# -*- coding: "utf8" -*-
+
+{
+    'name': 'Odoo Academy',
+    'summary': '''Academy app to manage Training''',
+    'description': '''
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attenders
+    ''',
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com',
+    'license': 'LGPL-3',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [
+        'demo/academy_demo.xml'
+    ]
+}
