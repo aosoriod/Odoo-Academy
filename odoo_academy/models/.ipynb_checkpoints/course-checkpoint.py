028ed47bfcf01dfd4836eeb174a07625982d3cf7
aosoriod <aosoriod@gmail.com>
2022-09-23 19:23:55 +0000

    [ADD] Basic Module

--- odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -4,19 +4,22 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
+    
     _name = 'academy.course'
     _description = 'Course Info'
-    
+
     name = fields.Char(string='Title', required=True)
-    description = fields.Text(string='Descripcion')
+    description = fields.Text(string='Description')
     
-    level = fields.Selection(string='level',
-                             selection=[('beginner', 'Beginner'),
-                                        ('intermediate', 'Intermediate'),
-                                        ('advanced', 'Advanced')],
-                             copy=False)
+    level = fields.Selection(string='Level',
+                            selection=[('beginner', 'Beginner'),
+                                       ('intermediate', 'Intermediate'),
+                                       ('advanced', 'Advanced')],
+                            copy=False)
+
     
     active = fields.Boolean(string='Active', default=True)
+    
     base_price = fields.Float(string='Base Price', default=0.00)
 
     additional_fee = fields.Float(string="Additional Fee", default=0.00)
@@ -34,4 +37,4 @@ class Course(models.Model):
     def _check_additional_fee(self):
         for record in self:
             if record.additional_fee < 10.00:
-                raise ValidationError(_('Additional Fees cannot be less than 10.00: %s' % record.additional_fee))
+                raise ValidationError(_('Additional Fees cannot be less than 10.00: %s' % record.additional_fee)) 
