7dbbbe7da16c6732a718ad56cf1bbda354a756ee
aosoriod <aosoriod@gmail.com>
2022-09-29 17:15:06 +0000

    [ADD Basic Module]

--- odoo_academy/models/session.py
+++ odoo_academy/models/session.py
@@ -42,7 +42,7 @@ class Session(models.Model):
     def _inverse_end_date(self):
         for record in self:
             if record.start_date and record.end_date:
-                record.duration = (record.end_date + record.start_date).days + 1
+                record.duration = (record.end_date - record.start_date).days + 1
             
             else:
                 continue
